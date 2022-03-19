135c569c7e6a2958c6c70854d6b95b4a95638a0d
StevenBildhauer <stevenbildhauer.sb@gmail.com>
2022-03-19 18:55:17 -0300

    aprendendo POO

---
+++ POO.php
@@ -0,0 +1,12 @@
+<?php
+
+$conta  = new contabanco();
+
+class contabanco {
+    private $saldo = 3245;
+    public $titular = 'Steven Bildhauer';
+    private $poupanca = 12563.34;
+
+}
+var_dump($conta->$saldo);
+
